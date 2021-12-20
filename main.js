80c8f511a59eff09d97e9eb82daf48b8dd327f06
Amina Madat <aminanba9@gmail.com>
2021-12-20 14:46:39 +0100

    correct the validation form

--- main.js
+++ main.js
@@ -17,24 +17,26 @@ scroll.addEventListener('click',()=>{
 const form= document.getElementById('form')
 const name = document.getElementById('name')
 
-email.addEventListener("keydown",(validation()))
-
-function validation(){
-  let email = document.getElementById('email').value
-  const pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3,4}$/;
+email.addEventListener("keydown",()=>{
+  let email = document.getElementById('email')
+  const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/i;
     let errorEl = document.getElementById('error')
-    if(email.match(pattern)){
+    if(email.value.match(pattern)){
       form.classList.add('valid')
       form.classList.remove('invalid')
       errorEl.textContent = "Your Email Address In Valid"
-      errorEl.style.color = "#00FF00"
+      errorEl.style.color = "#14b8a6"
     }else{
       form.classList.remove('valid')
       form.classList.add('invalid')
       errorEl.textContent = "Please Enter Valid Email Address"
       errorEl.style.color = "#FF0000"
     }
-}
+})
+
+
+  
+
 form.addEventListener('submit',(e)=>{
         e.preventDefault()
 })
